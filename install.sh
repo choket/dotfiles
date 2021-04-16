f6c8e8a2e6e10cc723558769c6d89a748fca11b3
root <root@localhost.localdomain>
2021-04-16 11:25:01 +0200

    Made install.sh set the permissions to 600 for all dotfiles

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ current_dir=$(dirname "$(readlink -f "$0")")
 ln -s "$current_dir/.zshrc" ~/.zshrc
 ln -s "$current_dir/.tmux.conf" ~/.tmux.conf
 ln -s "$current_dir/.vimrc" ~/.vimrc
+
+find . -type f -iname '.*' -exec chmod 600 {} \;
