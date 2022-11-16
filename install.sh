4c66665dfbeab31bff7d95aab84510b40b4e4fda
Stefan Stojanovski <47993064+choket@users.noreply.github.com>
2022-11-16 15:42:22 +0100

    add zsh plugins

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ ln -fs "$current_dir/.tmux.conf" ~/.tmux.conf
 ln -fs "$current_dir/.vimrc" ~/.vimrc
 
 find . -type f -iname '.*' -exec chmod 600 {} \;
+
+git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/zsh-syntax-highlighting
